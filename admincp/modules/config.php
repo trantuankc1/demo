92ed631a504a9a778d2b16fd105c71654d7e20c2
trantuankc1 <62491800+trantuankc1@users.noreply.github.com>
2022-03-03 18:18:45 +0700

    webdemo

---
+++ admincp/modules/config.php
@@ -0,0 +1,8 @@
+<?php
+	$tenmaychu='localhost';
+	$tentaikhoan='root';
+	$pass='';
+	$csdl='web.local';
+	$conn= mysqli_connect($tenmaychu,$tentaikhoan,$pass,$csdl) or die('Ko kết nối được');
+	
+?>
